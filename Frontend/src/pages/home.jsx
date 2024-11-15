534206230c1665715a4074746845da8d843d5daa
Maliha Tunzira <mtunjira@yahoo.com>
2024-11-15 13:13:44 +0100

    creating home page

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,10 +1,27 @@
 
 const Home = () => {
   return (
-    <div className='h-screen '>
-        <div className="grid grid-cols-4">
-            <div className="col-span-3">Right</div>
-            <div className="col-span-1">Left not</div>
+    <div className='h-screen bg-sky-600'>
+        <div className="grid grid-cols-4 p-10">
+            {/**Jumbotron on homepage if not logged in */}
+            <div className="col-span-3 text-left">
+                <h1 className="text-3xl">Welcome to DevDiaries!</h1>
+                <p className="text-lg">Here you can share your thoughts and stories on becoming a professional developer.</p>
+            </div>
+
+            {/**register form with log in link */}
+            <div className="col-span-1">
+                <div className="grid grid-cols-2 gap-2">
+                    <div className="col-span-1 space-y-2">
+                        <label htmlFor="FirstName:">First Name</label>
+                        <input type="text" name="fname" id="fname" className="rounded-lg w-auto" />
+                    </div>
+                    <div className="col-span-1">
+                        <label htmlFor="LastName:">Last Name</label>
+                        <input type="text" name="fname" id="lname" className="rounded-lg w-auto" />
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
