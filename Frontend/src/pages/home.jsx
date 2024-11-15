1e3853295c4f9979921149b5fb1c5e58b78b75ed
Maliha Tunzira <mtunjira@yahoo.com>
2024-11-15 13:00:42 +0100

    installing tailwindCSS

---
+++ Frontend/src/pages/home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <div className='h-screen '>
+        <div className="grid grid-cols-4">
+            <div className="col-span-3">Right</div>
+            <div className="col-span-1">Left not</div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
