ce7bf97a80577e5a9436ea70aa8a0cbd4c319aca
Maliha Tunzira <mtunjira@yahoo.com>
2024-11-17 12:44:30 +0100

    home background is not changing

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -10,8 +10,8 @@ createRoot(document.getElementById("root")).render(
     
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="/profile" element={<Profile/>}/>
+        <Route path="/" element={<App/>}/>
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,8 +1,7 @@
-
 const Home = () => {
   return (
     <div className='p-10'>
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-4 text-white">
             {/**Jumbotron on homepage if not logged in */}
             <div className="col-span-3 text-left">
                 <h1 className="text-3xl">Welcome to DevDiaries!</h1>
@@ -14,7 +13,7 @@ const Home = () => {
                 <div className="center">
                     <h3>Create an Account!</h3>
                 </div>
-                <div className="grid grid-cols-2 gap-2">
+                <div className="grid grid-cols-2 gap-2 bg-sky-700">
                     <div className="col-span-1">
                         <input type="text" name="fname" id="fname" placeholder="First Name" className="rounded-lg w-auto" />
                     </div>
@@ -22,7 +21,7 @@ const Home = () => {
                         <input type="text" name="lname" id="lname" placeholder="Last Name" className="rounded-lg w-auto" />
                     </div>
                     <div className="col-span-2">
-                        <input type="email" name="email" id="email" placeholder="Email" />
+                        <input type="email" name="email" id="email" placeholder="Email" className="rounded-lg w-full" />
                     </div>
                 </div>
             </div>
